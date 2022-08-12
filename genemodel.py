52390ac3727d078188ec8ad3b4245d362dcf075b
timchenyu <timchenyu2017@gmail.com>
2022-08-12 12:28:03 +0100

    added new graph struct

--- genemodel.py
+++ genemodel.py
@@ -5,6 +5,114 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def add_nodes(G, parent, num_to_add):
+    num_Nodes = G.number_of_nodes()
+    nodes_to_add = []
+    for n in range(1, 3):
+        nodes_to_add.append(num_Nodes+n)
+    for node in nodes_to_add:
+        G.add_edge(parent, node)
+
+def generate_gene_network(maxNodes, p_split, p_3):
+    """
+    generate a tree with depth n
+
+    each level can have a maximum of 6n + 1 nodes
+
+    first generate a center node
+        if max depth not reached:
+            generate 0-> 6n+1 nodes:
+                distribute the nodes across the nodes
+                nodes remaining = 6n+1
+                for nodes in this level:
+                    num child = random(0, nodes remaining)
+                    nodes remaining = nodes remanining - numchild
+
+    """
+    G = nx.path_graph(1)
+    num_Nodes = G.number_of_nodes()
+    parentNodes = []
+    newparents = []
+    if random.random() > p_3:
+        add_nodes(G, 0, 3)
+        num_Nodes = G.number_of_nodes()
+        for n in range(1, 3):
+            parentNodes.append(num_Nodes-n)
+    else:
+        add_nodes(G, 0, 2)
+        num_Nodes = G.number_of_nodes()
+        for n in range(1, 2):
+            parentNodes.append(num_Nodes-n)
+    count = 0
+    newparents = parentNodes
+    preNum = 111
+    while G.number_of_nodes() < maxNodes:
+        count += 1
+        #print(count)
+        print(G.number_of_nodes())
+        if preNum != G.number_of_nodes():
+            parentNodes = newparents
+            newparents = []
+        preNum = G.number_of_nodes()
+        for node in parentNodes:
+            if random.random() > p_split:
+                if random.random() > p_3:
+                    add_nodes(G, node, 5)
+                    num_Nodes = G.number_of_nodes()
+                    for n in range(1, 3):
+                        newparents.append(num_Nodes-n)
+                else:
+                    add_nodes(G, node, 3)
+                    num_Nodes = G.number_of_nodes()
+                    for n in range(1, 3):
+                        newparents.append(num_Nodes-n)
+
+        if preNum != G.number_of_nodes():
+            parentNodes = []
+    return G
+
+def makeDistanceMatrix(G, size):
+    Distance_Matrix = np.zeros((size, size))
+    for node in G.nodes():
+        p = nx.shortest_path(G, source=node)
+        for node1 in G.nodes():
+            if node != node1:
+                distance = len(p[node1])
+                Distance_Matrix[node-1][node1-1] = distance
+    return Distance_Matrix
+
+
+
+G = generate_gene_network(100, 0.3, 0.4)
+dm = makeDistanceMatrix(G, G.number_of_nodes())
+print(dm)
+pos = nx.spring_layout(G, seed=225)  # Seed for reproducible layout
+nx.draw(G)
+plt.show()
+exit()
+
+
+
+
+
+
+
+
+
+
+
+
+tree = nx.random_tree(n=100, seed=0)
+
+
+
+
+
+
+
+
+
+
 """
 定义一个玻尔兹曼机基因调控网络的类
 Define a Boltzmann Gene Regulatory Network Class
@@ -20,7 +128,7 @@ class BoltzmannGRN():
         self.matrix = make_wmatrix(self.graph, self.size)
         for node in self.graph.nodes:
             chance = random.random()
-            if chance > 0.99:
+            if chance > 1:
                 self.graph.nodes[node]["state"] = 1
             else:
                 self.graph.nodes[node]["state"] = 0
@@ -183,13 +291,13 @@ state_time = []
 time = []
 timestep = 0
 state_num = 0
-for t in range(0, 100):
-    """if t%5000 == 0:
+for t in range(0, 1000):
+    """if t > 0:
         #print(state_time)
         plt.rcParams["figure.figsize"] = [17.50, 3.50]
         plt.rcParams["figure.autolayout"] = True
-        plt.plot(time, state_time, 'r*')
-        plt.show()"""
+        plt.plot(time, list_size, 'r*')
+        #plt.show()"""
     time.append(timestep)
     timestep += 1
     states = []
@@ -222,7 +330,7 @@ for index, row in df.iterrows():
 
 print(state_time)
 plt.rcParams["figure.figsize"] = [17.50, 3.50]
-plt.rcParams["figure.autolayout"] = True
+#plt.rcParams["figure.autolayout"] = True
 plt.plot(time, list_size, 'r*')
 plt.show()
 
